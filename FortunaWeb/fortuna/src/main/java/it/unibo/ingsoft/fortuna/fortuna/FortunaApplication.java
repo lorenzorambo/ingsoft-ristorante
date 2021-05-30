a85e547d916dc555c08a35935a7daf45ec01880b
Filloax <filloax@gmail.com>
2021-05-30 20:39:17 +0200

    inizializzato progetto java/spring, include tomcat a quanto pare

---
+++ FortunaWeb/fortuna/src/main/java/it/unibo/ingsoft/fortuna/fortuna/FortunaApplication.java
@@ -0,0 +1,13 @@
+package it.unibo.ingsoft.fortuna.fortuna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FortunaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FortunaApplication.class, args);
+	}
+
+}
---
+++ FortunaWeb/fortuna/src/test/java/it/unibo/ingsoft/fortuna/fortuna/FortunaApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unibo.ingsoft.fortuna.fortuna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FortunaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
